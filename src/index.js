3860641cccb22919e7af271d0bcf3077d43e4935
informatica04 <gabriel.dias@somalogistica.com.br>
2023-11-30 14:28:47 -0300

    ajusteSwiper

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
 
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-
-
+  </React.StrictMode>
 );
 
